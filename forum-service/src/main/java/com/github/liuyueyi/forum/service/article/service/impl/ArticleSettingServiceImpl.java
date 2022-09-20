e592cb28f5ad59a64bfd09fb93b5be0511bc677a
louzai <514202273@qq.com>
2022-09-20 11:49:46 +0800

    feat:文章操作

--- forum-service/src/main/java/com/github/liuyueyi/forum/service/article/service/impl/ArticleSettingServiceImpl.java
+++ forum-service/src/main/java/com/github/liuyueyi/forum/service/article/service/impl/ArticleSettingServiceImpl.java
@@ -1,7 +1,18 @@
 package com.github.liuyueyi.forum.service.article.service.impl;
 
+import com.github.liueyueyi.forum.api.model.enums.OperateArticleTypeEnum;
+import com.github.liueyueyi.forum.api.model.vo.PageParam;
+import com.github.liueyueyi.forum.api.model.vo.PageVo;
+import com.github.liueyueyi.forum.api.model.vo.article.dto.ArticleDTO;
+import com.github.liueyueyi.forum.api.model.vo.article.dto.FlagBitDTO;
+import com.github.liuyueyi.forum.service.article.conveter.ArticleConverter;
+import com.github.liuyueyi.forum.service.article.repository.dao.ArticleDao;
+import com.github.liuyueyi.forum.service.article.repository.entity.ArticleDO;
 import com.github.liuyueyi.forum.service.article.service.ArticleSettingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 文章后台
@@ -12,4 +23,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class ArticleSettingServiceImpl implements ArticleSettingService {
 
+    @Autowired
+    private ArticleDao articleDao;
+
+    @Override
+    public PageVo<ArticleDTO> getArticleList(PageParam pageParam) {
+        List<ArticleDO> articleDOS = articleDao.listArticles(pageParam);
+        Integer totalCount = articleDao.countArticle();
+        return PageVo.build(ArticleConverter.toArticleDtoList(articleDOS),pageParam.getPageSize(), pageParam.getPageNum(),totalCount);
+    }
+
+    @Override
+    public void operateArticle(Long articleId, Integer operateType) {
+        ArticleDTO articleDTO = articleDao.queryArticleDetail(articleId);
+        Integer flagBit = articleDTO.getFlagBit();
+
+        FlagBitDTO flagBitDTO = OperateArticleTypeEnum.formCode(operateType).getFlagBit();
+        if (flagBitDTO.getForward().equals(Boolean.TRUE)) {
+            flagBit = flagBit | flagBitDTO.getFlagBit();
+        } else {
+            flagBit = flagBit & ~flagBitDTO.getFlagBit();
+        }
+        articleDao.updateArticleFlagBit(articleId, flagBit);
+    }
 }
--- forum-web/src/main/java/com/github/liuyueyi/forum/web/front/backstage/rest/ArticleSettingRestController.java
+++ forum-web/src/main/java/com/github/liuyueyi/forum/web/front/backstage/rest/ArticleSettingRestController.java
@@ -1,9 +1,9 @@
 package com.github.liuyueyi.forum.web.front.backstage.rest;
 
+import com.github.liueyueyi.forum.api.model.vo.ResVo;
 import com.github.liuyueyi.forum.service.article.service.ArticleSettingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 文章后台
@@ -17,4 +17,13 @@ public class ArticleSettingRestController {
 
     @Autowired
     private ArticleSettingService articleSettingService;
+
+    @ResponseBody
+    @GetMapping(path = "operate")
+    public ResVo<String> operate(@RequestParam(name = "articleId") Long articleId,
+                                 @RequestParam(name = "operateType") Integer operateType) {
+        // TODO：参数校验
+        articleSettingService.operateArticle(articleId, operateType);
+        return ResVo.ok("ok");
+    }
 }
