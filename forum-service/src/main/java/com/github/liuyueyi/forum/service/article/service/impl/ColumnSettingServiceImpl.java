5e23892751b00cb5cc32f77ad806c94ec95d62b0
louzai <514202273@qq.com>
2022-09-26 08:30:38 +0800

    feat:专栏

--- forum-service/src/main/java/com/github/liuyueyi/forum/service/article/service/impl/ColumnSettingServiceImpl.java
+++ forum-service/src/main/java/com/github/liuyueyi/forum/service/article/service/impl/ColumnSettingServiceImpl.java
@@ -1,7 +1,24 @@
 package com.github.liuyueyi.forum.service.article.service.impl;
 
+import com.github.liueyueyi.forum.api.model.vo.PageParam;
+import com.github.liueyueyi.forum.api.model.vo.PageVo;
+import com.github.liueyueyi.forum.api.model.vo.article.ColumnArticleReq;
+import com.github.liueyueyi.forum.api.model.vo.article.ColumnReq;
+import com.github.liueyueyi.forum.api.model.vo.article.dto.ColumnDTO;
+import com.github.liueyueyi.forum.api.model.vo.article.dto.SimpleArticleDTO;
+import com.github.liuyueyi.forum.core.util.NumUtil;
+import com.github.liuyueyi.forum.service.article.conveter.ColumnConvert;
+import com.github.liuyueyi.forum.service.article.repository.dao.ColumnDao;
+import com.github.liuyueyi.forum.service.article.repository.entity.ColumnArticleDO;
+import com.github.liuyueyi.forum.service.article.repository.entity.ColumnInfoDO;
+import com.github.liuyueyi.forum.service.article.repository.mapper.ColumnArticleMapper;
 import com.github.liuyueyi.forum.service.article.service.ColumnSettingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 专栏后台接口
@@ -11,4 +28,72 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ColumnSettingServiceImpl implements ColumnSettingService {
+
+    @Autowired
+    private ColumnServiceImpl columnService;
+
+    @Autowired
+    private ColumnDao columnDao;
+
+    @Autowired
+    private ColumnArticleMapper columnArticleMapper;
+
+    @Override
+    public void saveColumn(ColumnReq req) {
+        ColumnInfoDO columnInfoDO = ColumnConvert.toDo(req);
+        if (NumUtil.nullOrZero(req.getColumnId())) {
+            columnDao.save(columnInfoDO);
+        } else {
+            columnInfoDO.setId(req.getColumnId());
+            columnDao.updateById(columnInfoDO);
+        }
+    }
+
+    @Override
+    public void saveColumnArticle(ColumnArticleReq req) {
+        ColumnArticleDO columnArticleDO = ColumnConvert.toDo(req);
+        if (NumUtil.nullOrZero(req.getId())) {
+            columnArticleMapper.insert(columnArticleDO);
+        } else {
+            columnArticleDO.setId(req.getId());
+            columnArticleMapper.updateById(columnArticleDO);
+        }
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void sortColumnArticle(List<ColumnArticleReq> columnArticleReqs) {
+        columnArticleReqs.forEach(columnArticleReq -> {
+            ColumnArticleDO columnArticleDO = columnArticleMapper.selectById(columnArticleReq.getId());
+            columnArticleDO.setSection(columnArticleReq.getSection());
+            columnArticleMapper.updateById(columnArticleDO);
+        });
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void deleteColumn(Integer columnId) {
+        columnDao.deleteColumn(columnId);
+    }
+
+    @Override
+    public void deleteColumnArticle(Integer id) {
+        ColumnArticleDO columnArticleDO = columnArticleMapper.selectById(id);
+        if (columnArticleDO != null) {
+            columnArticleMapper.deleteById(id);
+        }
+    }
+
+    @Override
+    public PageVo<ColumnDTO> listColumn(PageParam pageParam) {
+        List<ColumnInfoDO> columnList = columnDao.listColumns(pageParam);
+        List<ColumnDTO> columnDTOS = ColumnConvert.toDtos(columnList);
+        Integer totalCount = columnDao.countColumns();
+        return PageVo.build(columnDTOS, pageParam.getPageSize(), pageParam.getPageNum(), totalCount);
+    }
+
+    @Override
+    public List<SimpleArticleDTO> queryColumnArticles(long columnId) {
+        return columnService.queryColumnArticles(columnId);
+    }
 }
--- forum-web/src/main/java/com/github/liuyueyi/forum/web/front/backstage/rest/ColumnSettingRestController.java
+++ forum-web/src/main/java/com/github/liuyueyi/forum/web/front/backstage/rest/ColumnSettingRestController.java
@@ -1,9 +1,14 @@
 package com.github.liuyueyi.forum.web.front.backstage.rest;
 
+import com.github.liueyueyi.forum.api.model.vo.ResVo;
+import com.github.liueyueyi.forum.api.model.vo.article.ColumnArticleReq;
+import com.github.liueyueyi.forum.api.model.vo.article.ColumnReq;
+import com.github.liueyueyi.forum.api.model.vo.banner.BannerReq;
 import com.github.liuyueyi.forum.service.article.service.ColumnSettingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 专栏后台
@@ -17,4 +22,39 @@ public class ColumnSettingRestController {
 
     @Autowired
     private ColumnSettingService columnSettingService;
+
+    @ResponseBody
+    @PostMapping(path = "saveColumn")
+    public ResVo<String> saveColumn(@RequestBody ColumnReq req) {
+        columnSettingService.saveColumn(req);
+        return ResVo.ok("ok");
+    }
+
+    @ResponseBody
+    @PostMapping(path = "saveColumnArticle")
+    public ResVo<String> saveColumnArticle(@RequestBody ColumnArticleReq req) {
+        columnSettingService.saveColumnArticle(req);
+        return ResVo.ok("ok");
+    }
+
+    @ResponseBody
+    @PostMapping(path = "sortColumnArticle")
+    public ResVo<String> sortColumnArticle(@RequestBody List<ColumnArticleReq> columnArticleReqs) {
+        columnSettingService.sortColumnArticle(columnArticleReqs);
+        return ResVo.ok("ok");
+    }
+
+    @ResponseBody
+    @GetMapping(path = "deleteColumn")
+    public ResVo<String> deleteColumn(@RequestParam(name = "columnId") Integer columnId) {
+        columnSettingService.deleteColumn(columnId);
+        return ResVo.ok("ok");
+    }
+
+    @ResponseBody
+    @GetMapping(path = "deleteColumnArticle")
+    public ResVo<String> deleteColumnArticle(@RequestParam(name = "id") Integer id) {
+        columnSettingService.deleteColumnArticle(id);
+        return ResVo.ok("ok");
+    }
 }
