c2c82a2a6cf25b9cf65aa29a85832979b050bd13
yihui <bangzewu@126.com>
2022-09-28 15:30:21 +0800

    feat:热门评论

--- forum-service/src/main/java/com/github/liuyueyi/forum/service/comment/service/impl/CommentReadServiceImpl.java
+++ forum-service/src/main/java/com/github/liuyueyi/forum/service/comment/service/impl/CommentReadServiceImpl.java
@@ -61,9 +61,7 @@ public class CommentReadServiceImpl implements CommentReadService {
         List<TopCommentDTO> result = new ArrayList<>();
         comments.forEach(comment -> {
             TopCommentDTO dto = topComments.get(comment.getId());
-            fillCommentInfo(dto);
-            dto.getChildComments().forEach(this::fillCommentInfo);
-            Collections.sort(dto.getChildComments());
+            fillTopCommentInfo(dto);
             result.add(dto);
         });
 
@@ -94,6 +92,17 @@ public class CommentReadServiceImpl implements CommentReadService {
     }
 
     /**
+     * 填充评论对应的信息
+     *
+     * @param comment
+     */
+    private void fillTopCommentInfo(TopCommentDTO comment) {
+        fillCommentInfo(comment);
+        comment.getChildComments().forEach(this::fillCommentInfo);
+        Collections.sort(comment.getChildComments());
+    }
+
+    /**
      * 填充评论对应的信息，如用户信息，点赞数等
      *
      * @param comment
@@ -118,6 +127,30 @@ public class CommentReadServiceImpl implements CommentReadService {
         // 查询点赞数
         Long praiseCount = countService.queryCommentPraiseCount(comment.getCommentId());
         comment.setPraiseCount(praiseCount.intValue());
+    }
+
+    /**
+     * 查询回帖最多的评论
+     *
+     * @param articleId
+     * @return
+     */
+    @Override
+    public TopCommentDTO queryHotComment(Long articleId) {
+        CommentDO comment = commentDao.getHotComment(articleId);
+        if (comment == null) {
+            return null;
+        }
+
+        TopCommentDTO result = CommentConverter.toTopDto(comment);
+        // 查询子评论
+        List<CommentDO> subComments = commentDao.listSubCommentIdMappers(articleId, Collections.singletonList(comment.getId()));
+        List<SubCommentDTO> subs = subComments.stream().map(CommentConverter::toSubDto).collect(Collectors.toList());
+        result.setChildComments(subs);
+
+        // 填充评论信息
+        fillTopCommentInfo(result);
+        return result;
     }
 
     @Override
