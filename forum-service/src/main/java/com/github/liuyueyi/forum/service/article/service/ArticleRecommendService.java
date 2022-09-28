68a22a30f755ed4721034b643e201bc6124ed782
yihui <bangzewu@126.com>
2022-09-28 13:02:35 +0800

    详情页关联推荐完成

--- forum-service/src/main/java/com/github/liuyueyi/forum/service/article/service/ArticleRecommendService.java
+++ forum-service/src/main/java/com/github/liuyueyi/forum/service/article/service/ArticleRecommendService.java
@@ -1,5 +1,7 @@
 package com.github.liuyueyi.forum.service.article.service;
 
+import com.github.liueyueyi.forum.api.model.vo.PageListVo;
+import com.github.liueyueyi.forum.api.model.vo.PageParam;
 import com.github.liueyueyi.forum.api.model.vo.article.dto.ArticleDTO;
 import com.github.liueyueyi.forum.api.model.vo.recommend.SideBarDTO;
 
@@ -11,7 +13,20 @@ import java.util.List;
  */
 public interface ArticleRecommendService {
 
+    /**
+     * 文章详情页的侧边栏推荐
+     *
+     * @param articleDO
+     * @return
+     */
     List<SideBarDTO> recommend(ArticleDTO articleDO);
 
 
+    /**
+     * 文章关联推荐
+     *
+     * @param article
+     * @return
+     */
+    PageListVo<ArticleDTO> relatedRecommend(Long article, PageParam pageParam);
 }
