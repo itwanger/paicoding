f5c683d2fcd44f0e9fe367e7c1cad8f3fc1b8b7d
yihui <bangzewu@126.com>
2022-10-15 21:31:53 +0800

    feat:自动创建表 & 启动成功后输出

--- forum-web/src/main/java/com/github/liuyueyi/forum/web/QuickForumApplication.java
+++ forum-web/src/main/java/com/github/liuyueyi/forum/web/QuickForumApplication.java
@@ -1,13 +1,15 @@
 package com.github.liuyueyi.forum.web;
 
+import com.github.liuyueyi.forum.core.util.SpringUtil;
+import com.github.liuyueyi.forum.web.config.GlobalViewConfig;
 import com.github.liuyueyi.forum.web.global.ForumExceptionHandler;
 import com.github.liuyueyi.forum.web.hook.interceptor.GlobalViewInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,9 +23,10 @@ import java.util.List;
  * @author yihui
  * @date 2022/7/6
  */
+@Slf4j
 @ServletComponentScan
 @SpringBootApplication
-public class QuickForumApplication implements WebMvcConfigurer {
+public class QuickForumApplication implements WebMvcConfigurer, ApplicationRunner {
 
     @Resource
     private GlobalViewInterceptor globalViewInterceptor;
@@ -42,4 +45,9 @@ public class QuickForumApplication implements WebMvcConfigurer {
         SpringApplication.run(QuickForumApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // 应用启动之后执行
+        log.info("启动成功，点击进入首页: {}", SpringUtil.getBean(GlobalViewConfig.class).getHost());
+    }
 }
---
+++ forum-web/src/main/java/com/github/liuyueyi/forum/web/config/init/ForumDataSourceInitializer.java
@@ -0,0 +1,57 @@
+package com.github.liuyueyi.forum.web.config.init;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.util.CollectionUtils;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+/**
+ * 表初始化，只有首次启动时，才会执行
+ *
+ * @author YiHui
+ * @date 2022/10/15
+ */
+@Configuration
+public class ForumDataSourceInitializer {
+    @Value("classpath:schema-all.sql")
+    private Resource schemaSql;
+    @Value("${database.name}")
+    private String database;
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        // 设置数据源
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        initializer.setEnabled(needInit(dataSource));
+        return initializer;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScripts(schemaSql);
+        populator.setSeparator(";");
+        return populator;
+    }
+
+    /**
+     * 检测一下数据库中表是否存在，若存在则不初始化；否则基于 schema-all.sql 进行初始化表
+     *
+     * @param dataSource
+     * @return
+     */
+    private boolean needInit(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        List list = jdbcTemplate.queryForList("SELECT table_name FROM information_schema.TABLES where table_name = 'user_info' and table_schema = '" + database + "';");
+        return CollectionUtils.isEmpty(list);
+    }
+}
