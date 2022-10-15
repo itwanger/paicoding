5df52edc1b8629ac43bbe267855e0c1d9ae3b406
yihui <bangzewu@126.com>
2022-10-15 23:05:29 +0800

    调整数据库初始化

--- forum-web/src/main/java/com/github/liuyueyi/forum/web/config/init/ForumDataSourceInitializer.java
+++ forum-web/src/main/java/com/github/liuyueyi/forum/web/config/init/ForumDataSourceInitializer.java
@@ -14,10 +14,7 @@ import org.springframework.util.CollectionUtils;
 
 import javax.sql.DataSource;
 import java.net.URI;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 
 /**
@@ -61,27 +58,45 @@ public class ForumDataSourceInitializer {
      * @return
      */
     private boolean needInit(DataSource dataSource) {
-        try {
-            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-            List list = jdbcTemplate.queryForList("SELECT table_name FROM information_schema.TABLES where table_name = 'user_info' and table_schema = '" + database + "';");
-            return CollectionUtils.isEmpty(list);
-        } catch (Exception e) {
-            // 查询失败，可能是数据库不存在，尝试创建数据库之后再次测试
-            URI url = URI.create(SpringUtil.getConfig("spring.datasource.url").substring(5));
-            String uname = SpringUtil.getConfig("spring.datasource.username");
-            String pwd = SpringUtil.getConfig("spring.datasource.password");
-            try (Connection connection = DriverManager.getConnection("jdbc:mysql://" + url.getHost() + ":" + url.getPort() +
-                    "?useUnicode=true&characterEncoding=UTF-8&useSSL=false", uname, pwd);
-                 Statement statement = connection.createStatement()) {
+        if (autoInitDatabase()) {
+            return true;
+        }
+        // 根据是否存在表来判断是否需要执行sql操作
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        List list = jdbcTemplate.queryForList("SELECT table_name FROM information_schema.TABLES where table_name = 'user_info' and table_schema = '" + database + "';");
+        return CollectionUtils.isEmpty(list);
+    }
+
+
+    /**
+     * 数据库不存在时，尝试创建数据库
+     */
+    private boolean autoInitDatabase() {
+        // 查询失败，可能是数据库不存在，尝试创建数据库之后再次测试
+        URI url = URI.create(SpringUtil.getConfig("spring.datasource.url").substring(5));
+        String uname = SpringUtil.getConfig("spring.datasource.username");
+        String pwd = SpringUtil.getConfig("spring.datasource.password");
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://" + url.getHost() + ":" + url.getPort() +
+                "?useUnicode=true&characterEncoding=UTF-8&useSSL=false", uname, pwd);
+             Statement statement = connection.createStatement()) {
+            ResultSet set = statement.executeQuery("select schema_name from information_schema.schemata where schema_name = '" + database + "'");
+            if (!set.next()) {
+                // 不存在时，创建数据库
                 String createDb = "CREATE DATABASE IF NOT EXISTS " + database;
                 connection.setAutoCommit(false);
                 statement.execute(createDb);
                 connection.commit();
                 log.info("创建数据库（{}）成功", database);
+                if (set.isClosed()) {
+                    set.close();
+                }
                 return true;
-            } catch (SQLException e2) {
-                throw new RuntimeException(e2);
             }
+            set.close();
+            log.info("数据库已存在，无需初始化");
+            return false;
+        } catch (SQLException e2) {
+            throw new RuntimeException(e2);
         }
     }
 }
