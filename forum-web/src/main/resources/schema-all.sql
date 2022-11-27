39e08b0be3c254fac0d07694d2e6dbd8e62c3210
yihui <bangzewu@126.com>
2022-11-27 15:53:49 +0800

    feat:更新数据库初始脚本

--- forum-web/src/main/resources/schema-all.sql
+++ forum-web/src/main/resources/schema-all.sql
@@ -295,3 +295,17 @@ CREATE TABLE `config`
     `update_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4  COMMENT='配置表';
+
+
+CREATE TABLE `dict_common` (
+   `id`             int unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+   `type_code`      varchar(100) NOT NULL DEFAULT '' COMMENT '字典类型，sex, status 等',
+   `dict_code`      varchar(100) NOT NULL DEFAULT '' COMMENT '字典类型的值编码',
+   `dict_desc`      varchar(200) NOT NULL DEFAULT '' COMMENT '字典类型的值描述',
+   `sort_no`        int(8) unsigned NOT NULL DEFAULT '0' COMMENT '排序编号',
+   `remark`         varchar(500) DEFAULT '' COMMENT '备注',
+   `create_time`    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+   `update_time`    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `uk_type_code_dict_code` (`type_code`,`dict_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='通用数据字典';
