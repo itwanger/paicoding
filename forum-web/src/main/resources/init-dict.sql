846f498c9e7c359747e601997e7a22e2043a96aa
louzai <514202273@qq.com>
2022-12-05 18:58:42 +0800

    feat:commit sql

--- forum-web/src/main/resources/init-dict.sql
+++ forum-web/src/main/resources/init-dict.sql
@@ -4,27 +4,20 @@ insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('C
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ConfigType','4','公告',4);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ConfigType','5','教程',5);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ConfigType','6','电子书',6);
-
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('PushStatus','0','未发布',1);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('PushStatus','1','已发布',2);
-
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ColumnStatus','0','未发布',1);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ColumnStatus','1','连载',2);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ColumnStatus','2','已完结',3);
-
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ArticleTag','1','热门',1);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ArticleTag','2','官方',2);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ArticleTag','3','推荐',3);
-
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ArticleSource','1','转载',1);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ArticleSource','2','原创',2);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ArticleSource','3','翻译',3);
-
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('OfficalStatus','0','非官方',1);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('OfficalStatus','1','官方',2);
-
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ToppingStatus','0','不置顶',1);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ToppingStatus','1','置顶',2);
-
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('CreamStatus','0','不加精',1);
 insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('CreamStatus','1','加精',2);
