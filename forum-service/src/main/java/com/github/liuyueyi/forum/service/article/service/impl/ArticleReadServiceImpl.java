46b7f3a5da2633a0876cca7f42da016e828eb77c
louzai <514202273@qq.com>
2022-12-16 21:13:19 +0800

    feat:添加 tag

--- forum-service/src/main/java/com/github/liuyueyi/forum/service/article/service/impl/ArticleReadServiceImpl.java
+++ forum-service/src/main/java/com/github/liuyueyi/forum/service/article/service/impl/ArticleReadServiceImpl.java
@@ -4,9 +4,11 @@ import com.github.liueyueyi.forum.api.model.enums.*;
 import com.github.liueyueyi.forum.api.model.exception.ExceptionUtil;
 import com.github.liueyueyi.forum.api.model.vo.PageListVo;
 import com.github.liueyueyi.forum.api.model.vo.PageParam;
+import com.github.liueyueyi.forum.api.model.vo.PageVo;
 import com.github.liueyueyi.forum.api.model.vo.article.dto.ArticleDTO;
 import com.github.liueyueyi.forum.api.model.vo.article.dto.CategoryDTO;
 import com.github.liueyueyi.forum.api.model.vo.article.dto.SimpleArticleDTO;
+import com.github.liueyueyi.forum.api.model.vo.article.dto.TagDTO;
 import com.github.liueyueyi.forum.api.model.vo.constants.StatusEnum;
 import com.github.liueyueyi.forum.api.model.vo.user.dto.BaseUserInfoDTO;
 import com.github.liuyueyi.forum.service.article.conveter.ArticleConverter;
@@ -61,6 +63,12 @@ public class ArticleReadServiceImpl implements ArticleReadService {
     @Override
     public ArticleDO queryBasicArticle(Long articleId) {
         return articleDao.getById(articleId);
+    }
+
+    @Override
+    public PageVo<TagDTO> queryTagsByArticleId(Long articleId) {
+        List<TagDTO> tagDTOS = articleTagDao.queryArticleTagDetails(articleId);
+        return PageVo.build(tagDTOS, 1, 10, tagDTOS.size());
     }
 
     @Override
