35e33227ca5aead2ad4428a4cb3d7e87a0e774d3
louzai <514202273@qq.com>
2022-12-24 18:07:50 +0800

    feat:修复保存为空问题

--- forum-web/src/main/java/com/github/liuyueyi/forum/web/front/image/rest/ImageRestController.java
+++ forum-web/src/main/java/com/github/liuyueyi/forum/web/front/image/rest/ImageRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServletRequest;
 
 /**
