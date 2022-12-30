b54b1fd0aabb351c37e98d17d333f9f283da8c98
yihui <bangzewu@126.com>
2022-12-30 15:18:39 +0800

    fix:图片上传失败异常提醒

--- forum-web/src/main/java/com/github/liuyueyi/forum/web/global/ForumExceptionHandler.java
+++ forum-web/src/main/java/com/github/liuyueyi/forum/web/global/ForumExceptionHandler.java
@@ -6,13 +6,15 @@ import com.github.liueyueyi.forum.api.model.vo.constants.StatusEnum;
 import com.github.liuyueyi.forum.core.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.core.NestedRuntimeException;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * 全局异常处理
@@ -22,29 +24,78 @@ import java.io.IOException;
  * @date 2022/9/3
  */
 @Slf4j
+@Order(-100)
 public class ForumExceptionHandler implements HandlerExceptionResolver {
     @Override
     public ModelAndView resolveException(HttpServletRequest request,
                                          HttpServletResponse response,
                                          Object handler, Exception ex) {
         log.error("unexpect error", ex);
-        String errMsg = ex instanceof ForumException ? ((ForumException) ex).getStatus().getMsg() : ExceptionUtils.getStackTrace(ex);
-        if (response.getContentType().contains(MediaType.APPLICATION_JSON_VALUE)) {
-            // 若是rest接口请求异常时，返回json格式的异常数据；而不是专门的500页面
-            // 访问需要登录的rest接口
-            response.setContentType("application/json");
+        String errMsg = buildToastMsg(ex);
+
+        if (restResponse(request, response)) {
+            // 表示返回json数据格式的异常提示信息
+            if (response.isCommitted()) {
+                // 如果返回已经提交过，直接退出即可
+                return new ModelAndView();
+            }
+
             try {
+                response.reset();
+                // 若是rest接口请求异常时，返回json格式的异常数据；而不是专门的500页面
+                response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+                response.setHeader("Cache-Control", "no-cache, must-revalidate");
                 response.getWriter().println(JsonUtil.toStr(ResVo.fail(StatusEnum.UNEXPECT_ERROR, errMsg)));
                 response.getWriter().flush();
-                return null;
-            } catch (IOException e) {
+                return new ModelAndView();
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
 
+        // 表示返回500页面
         ModelAndView mv = new ModelAndView("error/500");
         mv.getModel().put("toast", errMsg);
         response.setStatus(500);
         return mv;
+    }
+
+    private String buildToastMsg(Exception ex) {
+        if (ex instanceof ForumException) {
+            return ((ForumException) ex).getStatus().getMsg();
+        } else if (ex instanceof NestedRuntimeException) {
+            return ex.getMessage();
+        } else {
+            return ExceptionUtils.getStackTrace(ex);
+        }
+    }
+
+    /**
+     * 后台请求、api数据请求、上传图片等接口，返回json格式的异常提示信息
+     * 其他异常，返回500的页面
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    private boolean restResponse(HttpServletRequest request, HttpServletResponse response) {
+        if (request.getRequestURI().startsWith("admin/")) {
+            return true;
+        }
+
+        if (request.getRequestURI().startsWith("/image/upload")) {
+            return true;
+        }
+
+        if (response.getContentType() != null && response.getContentType().contains(MediaType.APPLICATION_JSON_VALUE)) {
+            return true;
+        }
+
+        // 数据接口请求
+        AntPathMatcher pathMatcher = new AntPathMatcher();
+        if (pathMatcher.match("/**/api/**", request.getRequestURI())) {
+            return true;
+        }
+        return false;
     }
 }
