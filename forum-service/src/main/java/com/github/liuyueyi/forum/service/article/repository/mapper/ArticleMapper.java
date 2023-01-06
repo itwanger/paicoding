1c4cfccea1203d37f6c26f17f35a39db5eba06c5
yihui <bangzewu@126.com>
2023-01-06 10:02:48 +0800

    fix:修复相关推荐重复问题 & 修复阅读计数问题 & 首页专栏推荐使用配置信息

--- forum-service/src/main/java/com/github/liuyueyi/forum/service/article/repository/mapper/ArticleMapper.java
+++ forum-service/src/main/java/com/github/liuyueyi/forum/service/article/repository/mapper/ArticleMapper.java
@@ -5,6 +5,7 @@ import com.github.liueyueyi.forum.api.model.vo.PageParam;
 import com.github.liueyueyi.forum.api.model.vo.article.dto.SimpleArticleDTO;
 import com.github.liueyueyi.forum.api.model.vo.article.dto.YearArticleDTO;
 import com.github.liuyueyi.forum.service.article.repository.entity.ArticleDO;
+import com.github.liuyueyi.forum.service.article.repository.entity.ReadCountDO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -42,9 +43,9 @@ public interface ArticleMapper extends BaseMapper<ArticleDO> {
      * @param pageParam
      * @return
      */
-    List<ArticleDO> listArticleByCategoryAndTags(@Param("categoryId") Long category,
-                                                        @Param("tags") List<Long> tagIds,
-                                                        @Param("pageParam") PageParam pageParam);
+    List<ReadCountDO> listArticleByCategoryAndTags(@Param("categoryId") Long category,
+                                                   @Param("tags") List<Long> tagIds,
+                                                   @Param("pageParam") PageParam pageParam);
 
     /**
      * 根据用户ID获取创作历程
