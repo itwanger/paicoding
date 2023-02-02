6d3fde3c6060b70d670a32a81ba59cb80343290d
louzai <514202273@qq.com>
2023-02-02 21:45:01 +0800

    feat:添加ip

--- paicoding-web/src/main/resources/liquibase/data/update_schema_230131.sql
+++ paicoding-web/src/main/resources/liquibase/data/update_schema_230131.sql
@@ -9,5 +9,5 @@ where id = 1;
 replace INTO `user` (id, third_account_id, user_name, password, login_type)
 VALUES (2, 'ojfU95x47r52Jtsl9q6tuljq85Pk', 'guest', '168128f21e4702eb07d39b7478602e64', 0);
 
-replace INTO user_info (id, user_id, user_name, photo, `position`, company, profile, user_role)
-VALUES (2, 2, '游客', 'https://cdn.tobebetterjavaer.com/paicoding/avatar/0010.png', '首席32', 'tech派', '我的简介', 0);
+replace INTO user_info (id, user_id, user_name, photo, `position`, company, profile, user_role, ip)
+VALUES (2, 2, '游客', 'https://cdn.tobebetterjavaer.com/paicoding/avatar/0010.png', '首席32', 'tech派', '我的简介', 0, '{"firstIp": "121.40.134.96", "latestIp": "58.48.23.111", "firstRegion": "浙江·杭州", "latestRegion": "湖北·武汉"}');
