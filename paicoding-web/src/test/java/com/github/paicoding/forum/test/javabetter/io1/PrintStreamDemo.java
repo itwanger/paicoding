8986198f3f4e20838075328ae715322bc633b0c4
沉默王二 <www.qing_gee@163.com>
2023-03-20 15:58:57 +0800

    IO示例

---
+++ paicoding-web/src/test/java/com/github/paicoding/forum/test/javabetter/io1/PrintStreamDemo.java
@@ -0,0 +1,7 @@
+package com.github.paicoding.forum.test.javabetter.io1;
+
+public class PrintStreamDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
