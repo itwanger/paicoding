2a62c010303210c56f0751338b20d879861f4830
qing-wq <2702461713@qq.com>
2023-04-17 22:53:46 +0800

    feat：添加评论删除和回复删除消息通知

--- paicoding-service/src/main/java/com/github/paicoding/forum/service/notify/service/impl/NotifyMsgListener.java
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/notify/service/impl/NotifyMsgListener.java
@@ -2,6 +2,7 @@ package com.github.paicoding.forum.service.notify.service.impl;
 
 import com.github.paicoding.forum.api.model.enums.NotifyStatEnum;
 import com.github.paicoding.forum.api.model.enums.NotifyTypeEnum;
+import com.github.paicoding.forum.api.model.enums.YesOrNoEnum;
 import com.github.paicoding.forum.api.model.vo.notify.NotifyMsgEvent;
 import com.github.paicoding.forum.core.util.SpringUtil;
 import com.github.paicoding.forum.service.article.repository.entity.ArticleDO;
@@ -46,8 +47,13 @@ public class NotifyMsgListener<T> implements ApplicationListener<NotifyMsgEvent<
             case COMMENT:
                 saveCommentNotify((NotifyMsgEvent<CommentDO>) msgEvent);
                 break;
+            case DELETE_COMMENT:
+                saveCommentNotify((NotifyMsgEvent<CommentDO>) msgEvent);
             case REPLY:
                 saveReplyNotify((NotifyMsgEvent<CommentDO>) msgEvent);
+                break;
+            case DELETE_REPLY:
+                removeReplyNotify((NotifyMsgEvent<CommentDO>) msgEvent);
                 break;
             case PRAISE:
             case COLLECT:
@@ -200,6 +206,38 @@ public class NotifyMsgListener<T> implements ApplicationListener<NotifyMsgEvent<
             // 若之前已经有对应的通知，则不重复记录；因为用户的关注是一对一的，可以重复的关注、取消，但是最终我们只通知一次
             notifyMsgDao.save(msg);
         }
+
+    }
+    /**
+     * 删除回复
+     * @param event
+     */
+    private void removeReplyNotify(NotifyMsgEvent<CommentDO> event) {
+        CommentDO comment = event.getContent();
+        CommentDO parent = commentReadService.queryComment(comment.getParentCommentId());
+        NotifyMsgDO msg = new NotifyMsgDO().setNotifyUserId(parent.getUserId())
+                .setOperateUserId(comment.getUserId())
+                .setRelatedId(comment.getArticleId())
+                .setType(NotifyTypeEnum.DELETE_REPLY.getType())
+                .setState(NotifyStatEnum.UNREAD.getStat())
+                .setMsg("用户删除回复");
+        notifyMsgDao.save(msg);
     }
 
+    /**
+     * 删除评论
+     * @param event
+     */
+    private void removeCommentNotify(NotifyMsgEvent<CommentDO> event) {
+        CommentDO comment = event.getContent();
+        ArticleDO article = articleReadService.queryBasicArticle(comment.getArticleId());
+        comment.setDeleted(YesOrNoEnum.YES.getCode());
+        NotifyMsgDO msg = new NotifyMsgDO().setNotifyUserId(article.getUserId())
+                .setOperateUserId(comment.getUserId())
+                .setRelatedId(comment.getArticleId())
+                .setType(NotifyTypeEnum.DELETE_COMMENT.getType())
+                .setState(NotifyStatEnum.UNREAD.getStat())
+                .setMsg(article.getTitle() +  ": 用户删除评论");
+        notifyMsgDao.save(msg);
+    }
 }
