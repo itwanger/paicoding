960868fd5bd4275f63db8cc47ce0c5e2e7ea00af
louzai <lvmenglou@xiaomi.com>
2023-04-22 17:09:14 +0800

    feat:接口优化

--- paicoding-web/src/main/java/com/github/paicoding/forum/web/admin/rest/ArticleSettingRestController.java
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/admin/rest/ArticleSettingRestController.java
@@ -27,7 +27,7 @@ import java.util.List;
  * @date 2022/9/19
  */
 @RestController
-//@Permission(role = UserRole.LOGIN)
+@Permission(role = UserRole.LOGIN)
 @Api(value = "文章设置管理控制器", tags = "文章管理")
 @RequestMapping(path = {"/api/admin/article/", "/admin/article/"})
 public class ArticleSettingRestController {
@@ -77,7 +77,7 @@ public class ArticleSettingRestController {
 
     @ApiOperation("文章搜索")
     @GetMapping(path = "query")
-    public ResVo<SearchArticleVo> queryArticleList(@RequestParam(name = "key", required = true) String key) {
+    public ResVo<SearchArticleVo> queryArticleList(@RequestParam(name = "key", required = false) String key) {
         List<SimpleArticleDTO> list = articleReadService.querySimpleArticleBySearchKey(key);
         SearchArticleVo vo = new SearchArticleVo();
         vo.setKey(key);
