3ecd043e47508faca782e256db019818ebbe6367
yanGaoLing <yangaoling0110@163.com>
2023-05-26 10:18:33 +0800

    首页查询引入ES，且兼容用户未安装ES查询

---
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/constant/EsFieldConstant.java
@@ -0,0 +1,24 @@
+package com.github.paicoding.forum.service.constant;
+
+/**
+ * ES 过滤字段常量
+ *
+ * @ClassName: EsFieldConstant
+ * @Author: ygl
+ * @Date: 2023/5/26 09:39
+ * @Version: 1.0
+ */
+public class EsFieldConstant {
+
+    /**
+     * title字段
+     */
+    public static final String ES_FIELD_TITLE = "title";
+
+    /**
+     * short_title字段
+     */
+    public static final String ES_FIELD_SHORT_TITLE = "short_title";
+
+
+}
