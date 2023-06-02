0e3130e51603431361b7b332d5fcfad0cba9da71
YiHui <bangzewu@126.com>
2023-06-02 19:48:53 +0800

    feat: 支持redis配置key，避免重启

--- paicoding-core/src/main/java/com/github/paicoding/forum/core/ai/ChatGptFactory.java
+++ paicoding-core/src/main/java/com/github/paicoding/forum/core/ai/ChatGptFactory.java
@@ -1,13 +1,17 @@
 package com.github.paicoding.forum.core.ai;
 
+import com.github.paicoding.forum.core.cache.RedisClient;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.plexpt.chatgpt.ChatGPT;
 import com.plexpt.chatgpt.ChatGPTStream;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.TimeUnit;
 
 
@@ -15,10 +19,21 @@ import java.util.concurrent.TimeUnit;
  * @author YiHui
  * @date 2023/4/20
  */
+@Slf4j
 @Service
 public class ChatGptFactory {
     @Value("${chatgpt.key}")
     private String key;
+
+    @PostConstruct
+    public void initKey() {
+        String rkey = RedisClient.getStr("chatgpt_key");
+        if (StringUtils.isNotBlank(rkey)) {
+            // redis 存在，则以redis为准
+            this.key = rkey;
+        }
+        log.info("当前选中key:{}, redisVal:{}", key, rkey);
+    }
 
     /**
      * 每个用户的会话缓存
