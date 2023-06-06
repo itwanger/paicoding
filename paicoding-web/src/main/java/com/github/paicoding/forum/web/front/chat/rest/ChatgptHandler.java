7de270ceb23cae082b5f33196820c1a76bff2432
itwanger <www.qing_gee@163.com>
2023-06-06 23:29:11 +0800

    聊天优化

--- paicoding-web/src/main/java/com/github/paicoding/forum/web/front/chat/rest/ChatgptHandler.java
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/front/chat/rest/ChatgptHandler.java
@@ -1,11 +1,16 @@
 package com.github.paicoding.forum.web.front.chat.rest;
 
+import cn.hutool.core.date.LocalDateTimeUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author YiHui
@@ -13,18 +18,32 @@ import java.time.LocalDateTime;
  */
 public class ChatgptHandler extends TextWebSocketHandler {
 
+    // 返回 TextMessage
+    private TextMessage getTextMessage(String msg) throws JsonProcessingException {
+        Map<String, String> map = new HashMap<>();
+        map.put("message", msg);
+        map.put("time", LocalDateTimeUtil.formatNormal(LocalDateTime.now()));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(map);
+
+        return new TextMessage(json);
+    }
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        session.sendMessage(new TextMessage("开始我们的AI之旅吧"));
+        session.sendMessage(getTextMessage("开始你和派聪明的AI之旅吧"));
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        session.sendMessage(new TextMessage(message.getPayload() + "\n--------------------\n返回结果: " + LocalDateTime.now()));
+        // 延迟 10 秒
+        Thread.sleep(10000);
+        session.sendMessage(getTextMessage(message.getPayload()));
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        session.sendMessage(new TextMessage("期待下次与您再会"));
+        session.sendMessage(getTextMessage("下次再撩吧（笑）"));
     }
 }
