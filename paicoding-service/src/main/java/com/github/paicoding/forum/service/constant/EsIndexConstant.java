302a6c3d8b614b55c2f1a1a3fc71de0f9cee0037
YiHui <bangzewu@126.com>
2023-06-09 19:14:55 +0800

    首页查询引入ES，且兼容用户未安装ES查询

---
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/constant/EsIndexConstant.java
@@ -0,0 +1,18 @@
+package com.github.paicoding.forum.service.constant;
+
+/**
+ * ES index
+ *
+ * @ClassName: EsFieldConstant
+ * @Author: ygl
+ * @Date: 2023/5/26 09:39
+ * @Version: 1.0
+ */
+public class EsIndexConstant {
+
+    /**
+     * article索引
+     */
+    public static final String ES_INDEX_ARTICLE = "article";
+
+}
