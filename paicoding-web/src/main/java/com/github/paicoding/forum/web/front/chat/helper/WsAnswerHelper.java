8bade310e6505ab7c569a7d545bc1232c4852ceb
YiHui <bangzewu@126.com>
2023-06-16 09:06:16 +0800

    feat: 合并工具类

--- paicoding-web/src/main/java/com/github/paicoding/forum/web/front/chat/helper/WsAnswerHelper.java
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/front/chat/helper/WsAnswerHelper.java
@@ -1,12 +1,17 @@
 package com.github.paicoding.forum.web.front.chat.helper;
 
+import com.github.paicoding.forum.api.model.context.ReqInfoContext;
 import com.github.paicoding.forum.api.model.enums.ai.AISourceEnum;
 import com.github.paicoding.forum.api.model.vo.chat.ChatRecordsVo;
+import com.github.paicoding.forum.core.mdc.MdcUtil;
 import com.github.paicoding.forum.service.chatai.ChatFacade;
+import com.github.paicoding.forum.service.user.service.SessionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * @author YiHui
@@ -46,5 +51,22 @@ public class WsAnswerHelper {
         // /user/username/chat/rsp，其中第二个参数 username 即为这里的第一个参数 session
         // username 也是AuthHandshakeHandler中配置的 Principal 用户识别标志
         simpMessagingTemplate.convertAndSendToUser(session, "/chat/rsp", response);
+    }
+
+    public void execute(Map<String, Object> attributes, Runnable func) {
+        try {
+            ReqInfoContext.ReqInfo reqInfo = (ReqInfoContext.ReqInfo) attributes.get(SessionService.SESSION_KEY);
+            ReqInfoContext.addReqInfo(reqInfo);
+            String traceId = (String) attributes.get(MdcUtil.TRACE_ID_KEY);
+            MdcUtil.add(MdcUtil.TRACE_ID_KEY, traceId);
+
+
+            // 执行具体的业务逻辑
+            func.run();
+
+        } finally {
+            ReqInfoContext.clear();
+            MdcUtil.clear();
+        }
     }
 }
