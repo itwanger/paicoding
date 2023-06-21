08ab2ad3c8fd272a027f1d37aff59bc22d60ed0a
yanGaoLing <yangaoling0110@163.com>
2023-06-21 23:03:31 +0800

    引入星球编号，支持用户名或星球编号登录

--- paicoding-web/src/test/java/com/github/paicoding/forum/test/ai/TestChatGPT.java
+++ paicoding-web/src/test/java/com/github/paicoding/forum/test/ai/TestChatGPT.java
@@ -9,12 +9,14 @@ public class TestChatGPT {
     public static void main(String[] args) {
         // export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
         //国内需要代理
-        Proxy proxy = Proxys.http("127.0.0.1", 7890);
+        // Proxy proxy = Proxys.http("127.0.0.1", 7892);
+        Proxy proxy = Proxys.http("10.3.4.136", 7892);
         //socks5 代理
         // Proxy proxy = Proxys.socks5("127.0.0.1", 1080);
 
         ChatGPT chatGPT = ChatGPT.builder()
-                .apiKey("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")
+//                .apiKey("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")
+                .apiKey("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")
                 .proxy(proxy)
                 .apiHost("https://api.openai.com/") //反向代理地址
                 .build()
