8e01b6346be74b9ac0b0ed011dbd33ec702614d2
YiHui <bangzewu@126.com>
2023-06-29 23:06:23 +0800

    ai配置化

--- paicoding-service/src/main/java/com/github/paicoding/forum/service/user/service/ai/UserAiServiceImpl.java
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/user/service/ai/UserAiServiceImpl.java
@@ -4,13 +4,14 @@ import com.github.paicoding.forum.api.model.enums.ai.AISourceEnum;
 import com.github.paicoding.forum.api.model.enums.user.UserAIStatEnum;
 import com.github.paicoding.forum.api.model.enums.user.UserAiStrategyEnum;
 import com.github.paicoding.forum.api.model.vo.chat.ChatItemVo;
+import com.github.paicoding.forum.core.util.DateUtil;
 import com.github.paicoding.forum.service.user.repository.dao.UserAiDao;
 import com.github.paicoding.forum.service.user.repository.dao.UserAiHistoryDao;
 import com.github.paicoding.forum.service.user.repository.entity.UserAiDO;
 import com.github.paicoding.forum.service.user.repository.entity.UserAiHistoryDO;
 import com.github.paicoding.forum.service.user.service.UserAiService;
+import com.github.paicoding.forum.service.user.service.conf.AiConfig;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.Objects;
@@ -23,13 +24,16 @@ public class UserAiServiceImpl implements UserAiService {
     @Resource
     private UserAiDao userAiDao;
 
+    @Resource
+    private AiConfig aiConfig;
+
     @Override
     public void pushChatItem(AISourceEnum source, Long user, ChatItemVo item) {
         UserAiHistoryDO userAiHistoryDO = new UserAiHistoryDO();
         userAiHistoryDO.setAiType(source.getCode());
         userAiHistoryDO.setUserId(user);
         userAiHistoryDO.setQuestion(item.getQuestion());
-        userAiHistoryDO.setAnswer(StringUtils.replace(item.getAnswer(), ChatItemVo.SPLIT_INFO, ""));
+        userAiHistoryDO.setAnswer(item.getAnswer());
         userAiHistoryDao.save(userAiHistoryDO);
     }
 
@@ -45,33 +49,33 @@ public class UserAiServiceImpl implements UserAiService {
         int cnt = 0;
         // 微信公众号登录用户 +5次
         if (UserAiStrategyEnum.WECHAT.match(strategy)) {
-            cnt += 5;
+            cnt += aiConfig.getMaxNum().getWechat();
         }
 
         // 星球用户 +100
         if (UserAiStrategyEnum.STAR_JAVA_GUIDE.match(strategy) || UserAiStrategyEnum.STAR_TECH_PAI.match(strategy)) {
             if (Objects.equals(ai.getState(), UserAIStatEnum.FORMAL.getCode())) {
                 // 审核通过
-                cnt += 100;
-            } else if (Objects.equals(ai.getState(), UserAIStatEnum.TRYING.getCode()) && (System.currentTimeMillis() - ai.getCreateTime().getTime()) <= (3 * 86400_000L)) {
+                cnt += aiConfig.getMaxNum().getStar();
+            } else if (Objects.equals(ai.getState(), UserAIStatEnum.TRYING.getCode()) && (System.currentTimeMillis() - ai.getCreateTime().getTime()) <= DateUtil.THREE_DAY_MILL) {
                 // 试用中
-                cnt += 30;
+                cnt += aiConfig.getMaxNum().getStarTry();
             }
         }
 
         // 推荐机制，如果绑定了邀请码，则总次数 + 10%
         if (UserAiStrategyEnum.INVITE_USER.match(strategy)) {
-            cnt = (int) (cnt * 1.1);
+            cnt = (int) (cnt * aiConfig.getMaxNum().getInvited());
         }
 
         // 根据推荐的人数，来进行增加
         if (ai.getInviteNum() > 0) {
-            cnt = (int) (cnt * ai.getInviteNum() * 1.2);
+            cnt = (int) (cnt * ai.getInviteNum() * aiConfig.getMaxNum().getInviteNum());
         }
 
         if (cnt == 0) {
-            // 对于登录用户，给5次使用机会
-            cnt = 5;
+            // 对于登录用户，给五次使用机会
+            cnt = aiConfig.getMaxNum().getBasic();
         }
         return cnt;
     }
