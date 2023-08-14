c9832d2b6517ca1fd61ab549f62bb2e84d613101
YiHui <bangzewu@126.com>
2023-08-14 20:42:18 +0800

    feat: 验证码获取

--- paicoding-web/src/main/java/com/github/paicoding/forum/web/front/login/wx/helper/WxLoginHelper.java
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/front/login/wx/helper/WxLoginHelper.java
@@ -96,6 +96,19 @@ public class WxLoginHelper {
         return sseEmitter;
     }
 
+    public String resend() throws IOException {
+        String deviceId = getOrInitDeviceId();
+        // 获取旧的验证码，注意不使用 getUnchecked, 避免重新生成一个验证码
+        String oldCode = deviceCodeCache.getIfPresent(deviceId);
+        SseEmitter lastSse = oldCode == null ? null : verifyCodeCache.getIfPresent(oldCode);
+        if (lastSse != null) {
+            lastSse.send("resend!");
+            lastSse.send("init#" + oldCode);
+            return "ok";
+        }
+        return "fail";
+    }
+
     /**
      * 刷新验证码
      *
