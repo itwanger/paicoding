ad888d6ea7f08b82213812204021e48b820f7540
YiHui <bangzewu@126.com>
2023-10-23 10:22:16 +0800

    fix: 长整型兼容

--- paicoding-web/src/main/resources/liquibase/data/update_schema_231018.sql
+++ paicoding-web/src/main/resources/liquibase/data/update_schema_231018.sql
@@ -11,11 +11,11 @@ alter table `read_count` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COM
 alter table `request_count` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID';
 
 
-alter table `notify_msg` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID', `related_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '关联的主键', `notify_user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '通知的用户id', `operate_user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '触发这个通知的用户id';
+alter table `notify_msg` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID', modify `related_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '关联的主键', modify `notify_user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '通知的用户id', modify `operate_user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '触发这个通知的用户id';
 
 alter table `user` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID';
-alter table `user_ai` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID', modify `user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户id', `inviter_user_id` bigint NOT NULL DEFAULT '0' COMMENT '当前用户绑定的邀请者';
+alter table `user_ai` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID', modify `user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户id', modify `inviter_user_id` bigint NOT NULL DEFAULT '0' COMMENT '当前用户绑定的邀请者';
 alter table `user_ai_history` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID', modify `user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户id';
-alter table `user_foot` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID', modify `user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户id', modify `document_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '文档ID（文章/评论）', `document_user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '发布该文档的用户ID';
+alter table `user_foot` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID', modify `user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户id', modify `document_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '文档ID（文章/评论）', modify `document_user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '发布该文档的用户ID';
 alter table `user_info` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID', modify `user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户id';
 alter table `user_relation` modify `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID', modify `user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户id', modify `follow_user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户id';
