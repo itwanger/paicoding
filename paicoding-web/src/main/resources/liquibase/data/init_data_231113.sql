9de98a1184cd331538c741db3c49e0ba8a0c8d0d
YiHui <bangzewu@126.com>
2023-11-13 17:07:37 +0800

    feat: 添加专栏教程枚举字典

---
+++ paicoding-web/src/main/resources/liquibase/data/init_data_231113.sql
@@ -0,0 +1,6 @@
+insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ColumnType','3','星球',4);
+
+insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ColumnArticleRead','0','沿用专栏的类型',1);
+insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ColumnArticleRead','1','登录阅读',2);
+insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ColumnArticleRead','2','免费',3);
+insert into dict_common(`type_code`,`dict_code`,`dict_desc`,`sort_no`) values('ColumnArticleRead','3','星球阅读',4);
