a960e8feb2d1cd94ecbdcd2deba82a0246a2f701
itwanger <www.qing_gee@163.com>
2024-03-07 16:04:05 +0800

    科学表达式

--- paicoding-web/src/test/java/com/github/paicoding/forum/test/javabetter/top/copydown/Html2md.java
+++ paicoding-web/src/test/java/com/github/paicoding/forum/test/javabetter/top/copydown/Html2md.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Slf4j
 public class Html2md {
     public static void main(String[] args) throws IOException {
-        String url = "https://mp.weixin.qq.com/s/fc48Z5tSMlBHweYIS1UL0g";
+        String url = "https://mp.weixin.qq.com/s/2nLk5wtj-AykKszl1wXnNA";
 
         // itwanger/Documents/GitHub/toBeBetterJavaer/docs/nice-article/
         HtmlSourceOption option = HtmlSourceOption.builder()
--- paicoding-web/src/test/java/com/github/paicoding/forum/test/zsxq/RandomDemo.java
+++ paicoding-web/src/test/java/com/github/paicoding/forum/test/zsxq/RandomDemo.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class RandomDemo {
     public static void main(String[] args) {
         System.out.println("恭喜这两位球友：");
-        // 一共 155 位球友留言，抽 2 位锦鲤送 60 元红包
-        System.out.println(Arrays.toString(NumberUtil.generateBySet(1,155,2)));
+        // 一共 332 位球友留言，抽 2 位锦鲤送 60 元红包
+        System.out.println(Arrays.toString(NumberUtil.generateBySet(1,332,2)));
     }
 }
