ca78027cfe5c849c5cb2d6250c3f3b8018ba9938
coldcode1 <481767803@qq.com>
2024-08-05 10:33:06 +0800

    微信扫码登陆尝试

--- paicoding-web/src/main/java/com/github/paicoding/forum/web/config/GlobalViewConfig.java
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/config/GlobalViewConfig.java
@@ -13,8 +13,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class GlobalViewConfig {
 
-    private String cdnImgStyle;
-
     private String websiteRecord;
 
     private Integer pageSize;
