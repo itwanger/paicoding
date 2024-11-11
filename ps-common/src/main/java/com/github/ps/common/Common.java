cc237b3287f3a7c3edd28cf72b33b8f0b6c6711a
xifeng_alven <xifeng_alven@foxmail.com>
2024-11-11 17:38:33 +0800

    添加common模块

---
+++ ps-common/src/main/java/com/github/ps/common/Common.java
@@ -0,0 +1,4 @@
+package com.github.ps.common;
+
+public class Common {
+}
---
+++ ps-common/src/test/java/com/github/ps/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ps.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
