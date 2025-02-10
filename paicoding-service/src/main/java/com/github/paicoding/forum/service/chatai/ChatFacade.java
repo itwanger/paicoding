c8aa24cdabcc7acef75ff9d142cb7bd716b4271e
betasecond <nbdhc9039@gmail.com>
2025-02-10 19:36:38 +0800

    Adds PaiAI and DouBaoAI as fallback AI sources. 添加AI source遗漏的选项

--- paicoding-service/src/main/java/com/github/paicoding/forum/service/chatai/ChatFacade.java
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/chatai/ChatFacade.java
@@ -89,6 +89,10 @@ public class ChatFacade {
                 source = AISourceEnum.XUN_FEI_AI;
             } else if (!except.contains(AISourceEnum.ALI_AI)) {
                 source = AISourceEnum.ALI_AI;
+            } else if(!except.contains(AISourceEnum.PAI_AI)) {
+                source = AISourceEnum.PAI_AI;
+            } else if(!except.contains(AISourceEnum.DOU_BAO_AI)) {
+                source = AISourceEnum.DOU_BAO_AI;
             } else {
                 source = AISourceEnum.PAI_AI;
             }
