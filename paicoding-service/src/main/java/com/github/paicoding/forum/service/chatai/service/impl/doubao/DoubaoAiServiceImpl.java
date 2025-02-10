9327f2e109ba019da6269b4ea7e817898efc6ae6
betasecond <nbdhc9039@gmail.com>
2025-02-10 10:50:47 +0800

    Refactors Doubao AI service to handle prompts and user messages. 添加对多轮对话及prompt的支持

--- paicoding-service/src/main/java/com/github/paicoding/forum/service/chatai/service/impl/doubao/DoubaoAiServiceImpl.java
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/chatai/service/impl/doubao/DoubaoAiServiceImpl.java
@@ -5,6 +5,7 @@ import com.github.paicoding.forum.api.model.enums.ai.AISourceEnum;
 import com.github.paicoding.forum.api.model.enums.ai.AiChatStatEnum;
 import com.github.paicoding.forum.api.model.vo.chat.ChatItemVo;
 import com.github.paicoding.forum.api.model.vo.chat.ChatRecordsVo;
+import com.github.paicoding.forum.service.chatai.constants.ChatConstants;
 import com.github.paicoding.forum.service.chatai.service.AbsChatService;
 import com.volcengine.ark.runtime.model.completion.chat.ChatCompletionRequest;
 import com.volcengine.ark.runtime.model.completion.chat.ChatMessage;
@@ -14,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
-
+import org.springframework.util.StringUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
@@ -27,6 +28,7 @@ public class DoubaoAiServiceImpl extends AbsChatService {
     private final ArkService service;
     @Autowired
     private DoubaoIntegration doubaoIntegration;
+
 
     @Autowired
     public DoubaoAiServiceImpl(DoubaoIntegration doubaoIntegration) {
@@ -62,7 +64,7 @@ public class DoubaoAiServiceImpl extends AbsChatService {
     @Override
     public AiChatStatEnum doAsyncAnswer(Long user, ChatRecordsVo chatRes, BiConsumer<AiChatStatEnum, ChatRecordsVo> consumer) {
         ChatItemVo item = chatRes.getRecords().get(0);
-        List<ChatMessage> messages = new ArrayList<>();
+        List<ChatMessage> messages = ChatConstants.toMsgList(chatRes.getRecords(), this::toMsg);
         messages.add(ChatMessage.builder().role(ChatMessageRole.SYSTEM).content("你是豆包，是由字节跳动开发的 AI 人工智能助手").build());
         messages.add(ChatMessage.builder().role(ChatMessageRole.USER).content(item.getQuestion()).build());
 
@@ -77,7 +79,7 @@ public class DoubaoAiServiceImpl extends AbsChatService {
                     consumer.accept(AiChatStatEnum.ERROR, chatRes);
                 })
                 .blockingForEach(choice -> {
-                    if (choice.getChoices().size() > 0) {
+                    if (!choice.getChoices().isEmpty()) {
                         item.appendAnswer((String) choice.getChoices().get(0).getMessage().getContent());
                         consumer.accept(AiChatStatEnum.MID, chatRes);
                     }
@@ -94,5 +96,21 @@ public class DoubaoAiServiceImpl extends AbsChatService {
     @Override
     public boolean asyncFirst() {
         return true;
+    }
+
+
+    private List<ChatMessage> toMsg(ChatItemVo item) {
+        List<ChatMessage> list = new ArrayList<>(2);
+        if (item.getQuestion().startsWith(ChatConstants.PROMPT_TAG)) {
+            // Prompt
+            list.add(ChatMessage.builder().role(ChatMessageRole.SYSTEM).content(item.getQuestion().substring(ChatConstants.PROMPT_TAG.length())).build());
+        } else {
+            // 用户提问和回答
+            list.add(ChatMessage.builder().role(ChatMessageRole.USER).content(item.getQuestion()).build());
+            if (StringUtils.hasText(item.getAnswer())) {
+                list.add(ChatMessage.builder().role(ChatMessageRole.ASSISTANT).content(item.getAnswer()).build());
+            }
+        }
+        return list;
     }
 }
