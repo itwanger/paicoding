562ee56b083adde9809915992c9c6cf01003d557
betasecond <nbdhc9039@gmail.com>
2025-02-12 22:08:41 +0800

    feat: Implements ShortLink data access layer with DAO and Mapper classes

---
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/shortlink/repository/ShortLinkDao.java
@@ -0,0 +1,9 @@
+package com.github.paicoding.forum.service.shortlink.repository;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.paicoding.forum.service.user.repository.entity.UserInfoDO;
+import com.github.paicoding.forum.service.user.repository.mapper.UserInfoMapper;
+
+public class ShortLinkDao extends ServiceImpl<UserInfoMapper, UserInfoDO> {
+
+}
---
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/shortlink/repository/ShortLinkMapper.java
@@ -0,0 +1,65 @@
+package com.github.paicoding.forum.service.shortlink.repository;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.paicoding.forum.api.model.vo.shortlink.ShortLinkDO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * 短链接服务mapper接口
+ * @author betasecond
+ * @date 2022-07-18
+ */
+interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+
+    /**
+     * 新建短链接
+     * @param shortLinkDO
+     * @return
+     */
+    @Insert("insert into short_link(original_url, short_url, username, third_party_user_id, user_agent, login_method, deleted, create_time, update_time) values(#{original_url}, #{short_url}, #{username}, #{third_party_user_id}, #{user_agent}, #{login_method}, #{deleted}, #{create_time}, #{update_time})")
+    int insert(ShortLinkDO shortLinkDO);
+
+    /**
+     * 根据短链接查询
+     * @param shortUrl
+     * @return
+     */
+    @Select("select * from short_link where short_url = #{short_url} limit 1")
+    ShortLinkDO getByShortUrl(@Param("short_url") String shortUrl);
+
+    /**
+     * 根据原始链接查询
+     * @param originalUrl
+     * @return
+     */
+    @Select("select * from short_link where original_url = #{original_url} limit 1")
+    ShortLinkDO getByOriginalUrl(@Param("original_url") String originalUrl);
+
+    /**
+     * 根据用户名查询
+     * @param username
+     * @return
+     */
+    @Select("select * from short_link where username = #{username}")
+    List<ShortLinkDO> getByUsername(@Param("username") String username);
+
+    /**
+     * 根据第三方用户id查询
+     * @param thirdPartyUserId
+     * @return
+     */
+    @Select("select * from short_link where third_party_user_id = #{third_party_user_id}")
+    List<ShortLinkDO> getByThirdPartyUserId(@Param("third_party_user_id") String thirdPartyUserId);
+
+    /**
+     * 根据userAgent查询
+     * @param userAgent
+     * @return
+     */
+    @Select("select * from short_link where user_agent = #{user_agent}")
+    List<ShortLinkDO> getByUserAgent(@Param("user_agent") String userAgent);
+}
