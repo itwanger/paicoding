78bdda45939229ad16390252e3ed7e0fd7a47e80
betasecond <nbdhc9039@gmail.com>
2025-02-14 16:47:58 +0800

    feat: Update short link schema and implement source detection logic 更新 ShortLinkRecord 表结构 实现了用户访问来源检测

--- paicoding-web/src/main/resources/liquibase/data/update_schema_250213.sql
+++ paicoding-web/src/main/resources/liquibase/data/update_schema_250213.sql
@@ -8,17 +8,18 @@ CREATE TABLE `short_link` (
     `deleted` TINYINT DEFAULT 0 NOT NULL comment '是否删除',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='短链接表';
+CREATE INDEX idx_short_code ON short_link(short_code);
 
 -- 短链接记录表
 CREATE TABLE `short_link_record` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-    `short_link_id` BIGINT UNSIGNED NOT NULL COMMENT '短链接ID',
+    `short_code` VARCHAR(255) NOT NULL COMMENT '短链接代码',
     `user_id` VARCHAR(255) COMMENT '用户ID',
     `access_time` BIGINT NOT NULL COMMENT '访问时间',
     `ip_address` VARCHAR(255) COMMENT 'IP地址',
     `login_method` VARCHAR(255) COMMENT '登录方式',
     `access_source` VARCHAR(255) COMMENT '访问来源',
     PRIMARY KEY (`id`),
-    KEY `idx_short_link_id` (`short_link_id`),
-    CONSTRAINT `fk_short_link_id` FOREIGN KEY (`short_link_id`) REFERENCES `short_link` (`id`)
+    KEY `idx_short_code` (`short_code`),
+    CONSTRAINT `fk_short_link_code` FOREIGN KEY (`short_code`) REFERENCES `short_link` (`short_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='短链接记录表';
