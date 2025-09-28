147ce96385567b4cdb4314e2c273d36f86aceb86
沉默王二 <www.qing_gee@163.com>
2025-09-28 15:10:12 +0800

    Update Zsxq bind link and default parameter handling
    Removed 'useXqName=false' from Zsxq bind links in user-related HTML templates and changed the default value of 'useXqName' to true in ZsxqLoginController. Added BooleanTest for testing BooleanUtils.toBoolean.

---
+++ paicoding-web/src/test/java/com/github/paicoding/forum/test/guavacache/BooleanTest.java
@@ -0,0 +1,9 @@
+package com.github.paicoding.forum.test.guavacache;
+
+import org.apache.commons.lang.BooleanUtils;
+
+public class BooleanTest {
+    public static void main(String[] args) {
+        System.out.println(BooleanUtils.toBoolean("true"));
+    }
+}
